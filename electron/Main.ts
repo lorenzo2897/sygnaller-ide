eaa9740d17611c2d72a33e80113f78183693d1c0
Lorenzo Silvestri <code@silvestrix.org>
2019-02-12 11:55:11 +0000

    Initial commit

---
+++ electron/Main.ts
@@ -0,0 +1,46 @@
+import { BrowserWindow } from 'electron';
+import * as path from "path";
+import * as url from "url";
+
+export default class Main {
+    static window: Electron.BrowserWindow;
+    static application: Electron.App;
+
+    private static onWindowAllClosed() {
+        if (process.platform !== 'darwin') {
+            Main.application.quit();
+        }
+    }
+
+    private static onClose() {
+        // Dereference the window object
+        Main.window = null;
+    }
+
+    private static onReady() {
+        Main.createWindow();
+    }
+
+    private static onActivate() {
+        Main.createWindow();
+    }
+
+    private static createWindow() {
+        Main.window = new BrowserWindow({ width: 800, height: 600 });
+        Main.window.loadURL(
+            url.format({
+                pathname: path.join(__dirname, `/../../dist/sygnaller/index.html`),
+                protocol: "file:",
+                slashes: true
+            })
+        );
+        Main.window.on('closed', Main.onClose);
+    }
+
+    static main(app: Electron.App) {
+        Main.application = app;
+        Main.application.on('window-all-closed', Main.onWindowAllClosed);
+        Main.application.on('activate', Main.onActivate);
+        Main.application.on('ready', Main.onReady);
+    }
+}
