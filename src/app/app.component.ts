81445d751e6e5c291bc1dcdbddc613353d239047
lorenzo2897 <code@silvestrix.org>
2019-02-13 19:59:03 +0000

    Add ace editor component to project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,39 @@
-import { Component, NgZone } from '@angular/core';
+import {AfterViewInit, Component, NgZone, ViewChild} from '@angular/core';
 import { ElectronService } from 'ngx-electron';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'sygnaller';
   fdata = 'File drop test';
 
+  @ViewChild('editor') editor;
+  editorMode: string = 'python';
+  editorTheme: string = 'eclipse';
+  editorText: string = "";
+  editorOptions = {
+    printMargin: false,
+    enableSnippets: true,
+    enableBasicAutocompletion: true,
+    enableLiveAutocompletion: true,
+    useSoftTabs: true
+  };
+
   constructor(private electron: ElectronService, private ngZone: NgZone) {}
+
+  ngAfterViewInit() {
+    this.editor.getEditor().commands.addCommand({
+      name: "showOtherCompletions",
+      bindKey: "Ctrl-.",
+      exec: function (editor) {
+
+      }
+    })
+  }
 
   onDrop(event) {
       event.preventDefault();
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgxElectronModule } from 'ngx-electron';
 import {SuiModule} from 'ng2-semantic-ui';
+import { AceEditorModule } from 'ng2-ace-editor';
 
 import { AppComponent } from './app.component';
 
@@ -12,7 +13,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     NgxElectronModule,
-    SuiModule
+    SuiModule,
+    AceEditorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
