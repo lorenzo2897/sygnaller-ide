1fea50d82ea8c85068211b3f7326f92f7426d21e
lorenzo2897 <code@silvestrix.org>
2019-05-20 20:08:53 +0100

    Populate getting started page

--- src/app/components/getting-started-page/getting-started-page.component.ts
+++ src/app/components/getting-started-page/getting-started-page.component.ts
@@ -1,4 +1,5 @@
 import {Component, HostBinding, Input, OnInit} from '@angular/core';
+import {ElectronService} from 'ngx-electron';
 
 @Component({
   selector: 'app-getting-started-page',
@@ -9,9 +10,17 @@ export class GettingStartedPageComponent implements OnInit {
 
   @Input() @HostBinding('class.dark') darkTheme: boolean = false;
 
-  constructor() { }
+  constructor(private electron: ElectronService) { }
 
   ngOnInit() {
+  }
+
+  openPynq() {
+    this.electron.shell.openExternal("https://pynq.readthedocs.io/en/latest/getting_started/pynq_z1_setup.html")
+  }
+
+  openDocs() {
+    this.electron.shell.openExternal("http://sygnaller.silvestri.io/")
   }
 
 }
